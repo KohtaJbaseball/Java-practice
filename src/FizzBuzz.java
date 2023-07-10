349b79241b4a24c3199d4eb010df4d33637ffe6d
KohtaTakashima <97589827+KohtaJbaseball@users.noreply.github.com>
2023-07-10 18:08:49 +0900

    [レビュー中]

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,7 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	
 		for (int i = 1; i < 101; i++) {
 			if (i % 15 == 0) {
 				System.out.println("FizzBuzz");
